bdf34a0fdce622d2a9ab28a27124187209b168b5
lenovo <lenovo@gmail.com>
2024-04-19 19:53:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once'includes/config_session.inc.php';
+require_once'includes/login.inc.php';
+require_once'includes/login_view.inc.php';
+?>
+
+<!DOCTYPE html >
+ <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    
+    <title> document </title>
+
+</head>
+
+<body>
+
+  <h3> login <h3>
+
+  <form action="includes/login.inc.php"  methode="post">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button> Login </button>
+  </form>
+
+  <?php
+  check_login_errors();
+  ?>
+
+</body>
+
+</html>
+
